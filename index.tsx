699682899ac3baa9d183c1f4673943db997ee3d3
Richard Wang <rwang83@gmail.com>
2021-03-25 02:02:11 -0400

    update

--- index.tsx
+++ index.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom"; 
+import * as ReactDOM from "react-dom";
+import { addShapeToScene } from './src/viewer';
+import { makeBottle } from './src/bottle';
 
 const App = () => (
   <h1>My React and TypeScript App!</h1>
 );
+
+
+let width = 50, height = 70, thickness = 30;
+makeBottle(width, height, thickness).then(addShapeToScene);
 
 ReactDOM.render(<App />, document.querySelector("#app"));
--- src/viewer.ts
+++ src/viewer.ts
@@ -7,32 +7,32 @@ import {
   Group,
   Scene
 } from 'three';
-import { initOpenCascade, OpenCascade, TopoDS_Shape } from "opencascade.js";
+import { initOpenCascade, TopoDS_Shape } from "opencascade.js";
 import { setupThreeJSViewport } from './lib/three-util';
 import visualize from './lib/visualize';
-import { makeBottle } from './bottle';
 
-const addShapeToScene = async (openCascade: OpenCascade, shape: TopoDS_Shape, scene: Scene) => {
-  const objectMat = new MeshStandardMaterial({
-    color: new Color(0.9, 0.9, 0.9)
-  });
 
-  let geometries = visualize(openCascade, shape);
-
-  let group = new Group();
-  geometries.forEach(geometry => {
-    group.add(new Mesh(geometry, objectMat));
-  });
-
-  group.name = "shape";
-  group.rotation.x = -Math.PI / 2;
-  scene.add(group);
-}
-
-const scene = setupThreeJSViewport();
-
-initOpenCascade().then(oc => oc.ready).then(openCascade => {
-  let width = 50, height = 70, thickness = 30;
-  let bottle = makeBottle(openCascade, width, height, thickness);
-  addShapeToScene(openCascade, bottle, scene);
+export const scene = setupThreeJSViewport();
+export const openCascadePromise = initOpenCascade().then(oc => {
+  console.log("opencascade wasm loaded");
+  return oc.ready;
 });
+export const addShapeToScene = (shape: TopoDS_Shape) => {
+  openCascadePromise.then(openCascade => {
+    const objectMat = new MeshStandardMaterial({
+      color: new Color(0.9, 0.9, 0.9)
+    });
+
+    let geometries = visualize(openCascade, shape);
+
+    let group = new Group();
+    geometries.forEach(geometry => {
+      group.add(new Mesh(geometry, objectMat));
+    });
+
+    group.name = "shape";
+    group.rotation.x = -Math.PI / 2;
+    scene.add(group);
+  });
+
+};
