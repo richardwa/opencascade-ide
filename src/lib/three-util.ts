d450556203b70a3f23effaeb8551f5b3e384f4f9
Richard Wang <rwang83@gmail.com>
2021-03-25 22:45:56 -0400

    update

--- src/lib/three-util.ts
+++ src/lib/three-util.ts
@@ -1,17 +1,5 @@
-import { OpenCascade, TopoDS_Shape } from 'opencascade.js';
-import {
-  AmbientLight,
-  DirectionalLight,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-  Color,
-  Geometry,
-  Mesh,
-  MeshStandardMaterial,
-} from 'three';
+import { AmbientLight, DirectionalLight, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { openCascadeHelper } from './opencascade-util';
 
 export const stats = {
   frameCount: [0],
@@ -63,21 +51,3 @@ export const setupThreeJSViewport = () => {
   return scene;
 }
 
-
-export const addShapeToScene = (shape: TopoDS_Shape, scene: Scene) => {
-  const facelist = openCascadeHelper.tessellate(shape);
-  const [locVertexcoord, locNormalcoord, locTriIndices] =  openCascadeHelper.joinPrimitives(facelist);
-  const tot_triangle_count = facelist.reduce((a, b) => a + b.number_of_triangles, 0);
-  const [vertices, faces] = openCascadeHelper.generateGeometry(tot_triangle_count, locVertexcoord, locNormalcoord, locTriIndices);
-
-  const objectMat = new MeshStandardMaterial({
-    color: new Color(0.9, 0.9, 0.9)
-  });
-  const geometry = new Geometry();
-  geometry.vertices = vertices;
-  geometry.faces = faces;
-  const object = new Mesh(geometry, objectMat);
-  object.name = "shape";
-  object.rotation.x = -Math.PI / 2;
-  scene.add(object);
-}
--- src/viewer.ts
+++ src/viewer.ts
@@ -21,21 +21,16 @@ const objectMat = new MeshStandardMaterial({
 });
 
 export const addShapeToScene = (shape: TopoDS_Shape) => {
-    const goemetries = openCascadeHelper.visualize(shape);
-    const group = new Group();
-    goemetries.forEach(({ vertices, normals, indices }) => {
-      const geometry = new BufferGeometry()
-      geometry.setAttribute('position',
-        new BufferAttribute(vertices, 3)
-      )
-      geometry.setAttribute('normal',
-        new BufferAttribute(normals, 3)
-      )
-      geometry.setIndex(new BufferAttribute(indices, 1))
-
-      group.add(new Mesh(geometry, objectMat));
-    });
-    group.name = "shape";
-    group.rotation.x = -Math.PI / 2;
-    scene.add(group);
+  const goemetries = openCascadeHelper.visualize(shape);
+  const group = new Group();
+  goemetries.forEach(({ vertices, normals, indices }) => {
+    const geometry = new BufferGeometry()
+    geometry.setAttribute('position', new BufferAttribute(vertices, 3))
+    geometry.setAttribute('normal', new BufferAttribute(normals, 3))
+    geometry.setIndex(new BufferAttribute(indices, 1))
+    group.add(new Mesh(geometry, objectMat));
+  });
+  group.name = "shape";
+  group.rotation.x = -Math.PI / 2;
+  scene.add(group);
 };
