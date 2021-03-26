2438ab2109a3b774d3d0ae0b7b53ef2483387e2e
Richard Wang <rwang83@gmail.com>
2021-03-25 22:09:52 -0400

    update

--- src/viewer.ts
+++ src/viewer.ts
@@ -5,34 +5,37 @@ import {
   Mesh,
   MeshStandardMaterial,
   Group,
-  Scene
+  Scene,
+  BufferGeometry,
+  BufferAttribute
 } from 'three';
-import { initOpenCascade, TopoDS_Shape } from "opencascade.js";
+import { TopoDS_Shape } from "opencascade.js";
+import { openCascadeHelper } from './lib/opencascade-util';
 import { setupThreeJSViewport } from './lib/three-util';
-import visualize from './lib/visualize';
 
 
 export const scene = setupThreeJSViewport();
-export const openCascadePromise = initOpenCascade().then(oc => {
-  console.log("opencascade wasm loaded");
-  return oc.ready;
+
+const objectMat = new MeshStandardMaterial({
+  color: new Color(0.9, 0.9, 0.9)
 });
+
 export const addShapeToScene = (shape: TopoDS_Shape) => {
-  openCascadePromise.then(openCascade => {
-    const objectMat = new MeshStandardMaterial({
-      color: new Color(0.9, 0.9, 0.9)
-    });
+    const goemetries = openCascadeHelper.visualize(shape);
+    const group = new Group();
+    goemetries.forEach(({ vertices, normals, indices }) => {
+      const geometry = new BufferGeometry()
+      geometry.setAttribute('position',
+        new BufferAttribute(vertices, 3)
+      )
+      geometry.setAttribute('normal',
+        new BufferAttribute(normals, 3)
+      )
+      geometry.setIndex(new BufferAttribute(indices, 1))
 
-    let geometries = visualize(openCascade, shape);
-
-    let group = new Group();
-    geometries.forEach(geometry => {
       group.add(new Mesh(geometry, objectMat));
     });
-
     group.name = "shape";
     group.rotation.x = -Math.PI / 2;
     scene.add(group);
-  });
-
 };
