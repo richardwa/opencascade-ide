e185f4980fd65beeb22a5fb0cdfbfbd80280b43b
Richard Wang <rwang83@gmail.com>
2021-03-25 22:45:47 -0400

    update

--- index.tsx
+++ index.tsx
@@ -4,17 +4,33 @@ import { addShapeToScene } from './src/viewer';
 import { makeBottle } from './src/bottle';
 import { stats } from './src/lib/three-util'
 
+type State = {
+  fps: number;
+}
+type Props = {
 
-const App = () => {
-  const last = stats.frameCount.length - 1;
-  const fps = stats.frameCount[last - 1];
+}
+class App extends React.Component<Props, State> {
+  constructor(p: Props) {
+    super(p);
+    this.state = {
+      fps: 0
+    };
+    setInterval(() => {
+      const frameCount = stats.frameCount;
+      const penult = frameCount.length - 2;
+      this.setState({ fps: stats.frameCount[penult] });
+    }, 1000);
 
-  return <h1>fps {fps.toFixed(0)} </h1>;
-};
+    let width = 50, height = 70, thickness = 30;
+    makeBottle(width, height, thickness).then(addShapeToScene);
+  }
 
-let width = 50, height = 70, thickness = 30;
-makeBottle(width, height, thickness).then(addShapeToScene);
 
-setInterval(() => {
-  ReactDOM.render(<App />, document.querySelector("#app"));
-}, 1000);
+  render() {
+    return <p>fps {this.state.fps.toFixed(0)} </p>;
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector("#app"));
